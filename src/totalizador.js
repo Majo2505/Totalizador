c9bc50d77d98cdc202a7e1eaefe2f6ce58c7d221
Your Name <you@example.com>
2026-09-08 09:50:23 -0400

    feat: calcular impuesto para el estado de Utah

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,4 +1,13 @@
 export function calcularPrecioNeto(cantidad, precioPorItem) 
 {
-  return cantidad * precioPorItem;
+    return cantidad * precioPorItem;
+}
+
+export function calcularImpuesto(precioNeto, estado) {
+    if (estado === "UT")
+
+    {
+        return precioNeto * 0.0665
+    }
+    return 0;
 }
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,13 @@
-import {calcularPrecioNeto} from "./totalizador.js";
+import {calcularPrecioNeto, calcularImpuesto} from "./totalizador.js";
 
 describe("CalcularPrecioNeto", () => {
-  it("deberia calcular el precio neto multiplicando cantidad por precio", () => {
-    expect(calcularPrecioNeto(20, 3)).toEqual(60);
-  });
+    it("deberia calcular el precio neto multiplicando cantidad por precio", () => {
+        expect(calcularPrecioNeto(20, 3)).toEqual(60);
+    });
+});
+
+describe("CalcularImpuesto", () => {
+    it("deberia calcular el impuesto del 6.65% para el estado de UT", () => {
+        expect(calcularImpuesto(60, "UT")).toEqual(3.99);
+    });
 });
